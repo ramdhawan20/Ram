5df2b219037f2b3f023ef67011a19d9b4f3f0616
Ranjankumar Yadav <ranjankumar.y@HCL.COM>
2019-02-28 15:13:02 +0530

    Changes for csv header and constants

--- backend/HCL-BSS/src/main/java/com/hcl/bss/constants/ApplicationConstants.java
+++ backend/HCL-BSS/src/main/java/com/hcl/bss/constants/ApplicationConstants.java
@@ -10,21 +10,33 @@ public class ApplicationConstants {
     public static final String COMMA_DELIMITER = ",";
      
     //Product attributes index
-    public static final int PRODUCT_ID_IDX = 0;
+    public static final int PRODUCT_TYPE_CODE_IDX = 0;
     public static final int PRODUCT_DISPLAY_NAME_IDX = 1;
     public static final int SKU_IDX = 2;
     public static final int EXP_DATE_IDX = 3; 
     public static final int PROD_DESCRIPTION_IDX = 4;
+    public static final int START_DATE_IDX = 5;
     public static final String BLANK = "";
-    public static final String DATE_FORMAT_MMDDYYYY = "MM/dd/yyyy";
+    public static final String DATE_FORMAT_DDMMYYYY = "dd/MM/yyyy";
+    public static final String DATE_FORMAT_DD_MM_YYYY = "dd-MM-yyyy";
     public static final String STATUS_PARTIAL_SUCCESS = "Partial Success";
     public static final String STATUS_SUCCESS = "Success";
     public static final String STATUS_FAIL = "Fail";
     public static final String EXTERNAL_FILE_PATH = "C://Users//Public//temp//";
     public static final String FILE_NOT_EXIST_MSG = "Sorry. The file you are looking for does not exist";
     public static final String ERROR_FILE_NAME_SUFFIX = "yyyy-MM-dd-HH_mm_ss";
-    public static String UPLOADED_FOLDER = "C://Users//Public//temp//";
-    public static String ERROR_FILE = "C:/Users/Public/temp/error_log_";
-    public static String CSV_EXTENTION = ".csv";
+    public static final String UPLOADED_FOLDER = "C://Users//Public//temp//";
+    public static final String ERROR_FILE = "C:/Users/Public/temp/error_log_";
+    public static final String CSV_EXTENTION = ".csv";
+    public static final String TEXT_EXTENTION = ".txt";
     public static final String NEW_LINE_SEPARATOR = "\n";
+    public static final String CSV_WRONG_DATA = "Incorrect CSV Header or No Records in file ! Download Sample CSV!";
+    public static final String INVALID_FILE_TYPE = "Invalid File Type! Only CSV Allowed.";
+    public static final String DD_MM_YYYY_HH_MM = "dd-MM-yyyy.HH.mm";
+    public static final String DD_MM_YYYY = "dd/MM/yyyy";
+    public static final String ROW_NO = "Row No: ";
+    public static final String TAB_SPACE = "\t";
+    public static final String ERROR = "Error : ";
+    public static final String LINE_SEPERATOR = "line.separator";
+    public static final String UTF_8 = "UTF-8";
 }
