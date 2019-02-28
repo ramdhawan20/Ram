64d1479bdba633718bf5430320c72a0e7cfdc446
Ranjankumar Yadav <ranjankumar.y@HCL.COM>
2019-02-28 14:54:44 +0530

    Product persistence changes

--- backend/HCL-BSS/src/main/java/com/hcl/bss/domain/ProductTypeMaster.java
+++ backend/HCL-BSS/src/main/java/com/hcl/bss/domain/ProductTypeMaster.java
@@ -1,90 +1,109 @@
 package com.hcl.bss.domain;
 
-import org.hibernate.annotations.Proxy;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.io.Serializable;
-import java.sql.Timestamp;
-/**
- *
- * @author- Aditya gupta
- */
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 @Entity
-@Table(name="TB_PRODUCT_TYPE_MASTER")
-@Proxy(lazy=false)
-public class ProductType implements Serializable {
+@Table(name = "TB_PRODUCT_TYPE_MASTER")
+public class ProductTypeMaster implements java.io.Serializable {
 
-    @Id
-    @Column(name="PRODUCT_TYPE_CODE")
-    private String typeCode;
-    @Column(name="PRODUCT_TYPE")
-    private String typeName;
-
-    @CreatedBy
-    @Column(name = "CRE_BY")
-    private String createdBy;
-    @CreatedDate
-    @Column(name = "CRE_DT")
-    private Timestamp createdDate;
-    @Column(name = "UPD_BY")
-    @LastModifiedBy
-    private String updatedBy;
-    @LastModifiedDate
-    @Column(name = "UPD_DT")
-    private Timestamp updatedDate;
-
-    public String getTypeCode() {
-        return typeCode;
-    }
-
-    public void setTypeCode(String typeCode) {
-        this.typeCode = typeCode;
-    }
-
-    public String getTypeName() {
-        return typeName;
-    }
-
-    public void setTypeName(String typeName) {
-        this.typeName = typeName;
-    }
-
-    public Timestamp getCreatedDate() {
-        return createdDate;
-    }
-
-    public void setCreatedDate(Timestamp createdDate) {
-        this.createdDate = createdDate;
-    }
-
-    public String getCreatedBy() {
-        return createdBy;
-    }
-
-    public void setCreatedBy(String createdBy) {
-        this.createdBy = createdBy;
-    }
-
-    public Timestamp getUpdatedDate() {
-        return updatedDate;
-    }
-
-    public void setUpdatedDate(Timestamp updatedDate) {
-        this.updatedDate = updatedDate;
-    }
-
-    public String getUpdatedBy() {
-        return updatedBy;
-    }
-
-    public void setUpdatedBy(String updatedBy) {
-        this.updatedBy = updatedBy;
-    }
+	private static final long serialVersionUID = 1L; 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	/*@OneToMany(mappedBy="productTypeCode", cascade = CascadeType.ALL)
+	Set product = new HashSet();*/
+	@Column(name = "PRODUCT_TYPE_CODE", nullable = false)
+	private String productTypeCode;
+	@Column(name = "PRODUCT_TYPE", nullable = false, length = 45)
+	private String productType;
+	@Column(name = "DESC",length = 100)
+	private String discription;
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "CRE_DT", nullable = false, length = 3)
+	private Date createdDate;
+	@Column(name = "CRE_BY", length = 50)
+	private String createdBy;
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "UPD_DT", nullable = false, length = 3)
+	private Date updatedDate;
+	@Column(name = "UPD_BY", length = 50)
+	private String updatedBy;
+	public ProductTypeMaster() {
+		super();
+	}
+	public ProductTypeMaster(String productType, String discription, Date createdDate, String createdBy,
+			Date updatedDate, String updatedBy) {
+		super();
+		this.productType = productType;
+		this.discription = discription;
+		this.createdDate = createdDate;
+		this.createdBy = createdBy;
+		this.updatedDate = updatedDate;
+		this.updatedBy = updatedBy;
+	}
+	@Override
+	public String toString() {
+		return "ProductTypeMaster [productTypeCode=" + productTypeCode + ", productType=" + productType
+				+ ", discription=" + discription + ", createdDate=" + createdDate + ", createdBy=" + createdBy
+				+ ", updatedDate=" + updatedDate + ", updatedBy=" + updatedBy + "]";
+	}
+	public String getProductTypeCode() {
+		return productTypeCode;
+	}
+	public void setProductTypeCode(String productTypeCode) {
+		this.productTypeCode = productTypeCode;
+	}
+	public String getProductType() {
+		return productType;
+	}
+	public void setProductType(String productType) {
+		this.productType = productType;
+	}
+	public String getDiscription() {
+		return discription;
+	}
+	public void setDiscription(String discription) {
+		this.discription = discription;
+	}
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Date getUpdatedDate() {
+		return updatedDate;
+	}
+	public void setUpdatedDate(Date updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+	/*public Set getProduct() {
+		return product;
+	}
+	public void setProduct(Set product) {
+		this.product = product;
+	}*/
+	
+	
+	
 }
--- backend/HCL-BSS/src/main/java/com/hcl/bss/repository/ProductRepository.java
+++ backend/HCL-BSS/src/main/java/com/hcl/bss/repository/ProductRepository.java
@@ -1,56 +1,11 @@
 package com.hcl.bss.repository;
 
-import java.io.Serializable;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-
-import javax.transaction.Transactional;
-
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.query.Query;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import com.hcl.bss.dao.ProductDAO;
-import com.hcl.bss.domain.ErrorCsvFile;
 import com.hcl.bss.domain.Product;
-import com.hcl.bss.dto.ProductDto;
+
 @Repository
-@Transactional
-public class ProductRepository implements ProductDAO {
-	
-	
-	 private SessionFactory sessionFactory;
-	    @Autowired
-	    public ProductRepository(SessionFactory sessionFactory) {
-	        this.sessionFactory = sessionFactory;
-	    }
-	@Override
-	public List<ProductDto> saveProduct(List<ProductDto> listProduct) throws ParseException {
-		
-		 Session session = this.sessionFactory.getCurrentSession();
-		
-		
-		for(ProductDto prod :listProduct) {
-			Product product = new Product();
-			product.setProductId(prod.getProductId());
-			product.setSku(prod.getSku());
-			product.setProductDispName(prod.getProductDispName());
-			product.setCreatedDate(new Date());
-			String expDate = prod.getProductExpDate();
-			Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(expDate);  
-			product.setProductExpDate(date1);
-			session.save(product);
-			System.out.println(product.toString());
-			
-		}
-		
-		return listProduct;
-	}
-	
-	
+public interface ProductRepository extends CrudRepository<Product, Long> {
 
 }
