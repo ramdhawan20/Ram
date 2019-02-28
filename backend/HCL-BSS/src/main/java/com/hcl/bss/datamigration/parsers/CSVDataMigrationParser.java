7219ba6df4b01f33352ca862ae1dbbbf290c3c2c
Ranjankumar Yadav <ranjankumar.y@HCL.COM>
2019-02-28 14:50:50 +0530

    Column number count error fix

--- backend/HCL-BSS/src/main/java/com/hcl/bss/datamigration/parsers/CSVDataMigrationParser.java
+++ backend/HCL-BSS/src/main/java/com/hcl/bss/datamigration/parsers/CSVDataMigrationParser.java
@@ -1,33 +1,37 @@
 package com.hcl.bss.data.migration.parsers;
 
+import static com.hcl.bss.constants.ApplicationConstants.BLANK;
+import static com.hcl.bss.constants.ApplicationConstants.COMMA_DELIMITER;
+import static com.hcl.bss.constants.ApplicationConstants.EXP_DATE_IDX;
+import static com.hcl.bss.constants.ApplicationConstants.PRODUCT_DISPLAY_NAME_IDX;
+import static com.hcl.bss.constants.ApplicationConstants.PRODUCT_TYPE_CODE_IDX;
+import static com.hcl.bss.constants.ApplicationConstants.PROD_DESCRIPTION_IDX;
+import static com.hcl.bss.constants.ApplicationConstants.SKU_IDX;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import com.hcl.bss.dao.ProductDAO;
 import com.hcl.bss.dto.ProductDto;
 import com.hcl.bss.exceptions.ImportParseException;
 import com.hcl.bss.validator.DataMigrationFieldValidator;
 
-import static com.hcl.bss.constants.ApplicationConstants.COMMA_DELIMITER;
-import static com.hcl.bss.constants.ApplicationConstants.PRODUCT_ID_IDX;
-import static com.hcl.bss.constants.ApplicationConstants.PRODUCT_DISPLAY_NAME_IDX;
-import static com.hcl.bss.constants.ApplicationConstants.SKU_IDX;
-import static com.hcl.bss.constants.ApplicationConstants.EXP_DATE_IDX;
-import static com.hcl.bss.constants.ApplicationConstants.PROD_DESCRIPTION_IDX;
-import static com.hcl.bss.constants.ApplicationConstants.BLANK;
-
 @Component
 public class CSVDataMigrationParser {
+	private static final Logger LOGGER = LoggerFactory.getLogger(CSVDataMigrationParser.class);
 	@Autowired
 	DataMigrationFieldValidator dataMigrationFieldValidator;
-	@Autowired
-	ProductDAO productDAO;
+	@Value("${upload.file.header}")
+	String prescribedFileHeader;
 
 	public List<ProductDto> parseCsvData(String fileName) throws IOException, ParseException {
 		List<ProductDto> listProduct = new ArrayList<>();
@@ -40,42 +44,62 @@ public class CSVDataMigrationParser {
 			// Create the file reader
 			fileReader = new BufferedReader(new FileReader(fileName));
 
-			// Read the CSV file header to skip it
-			fileReader.readLine();
+			// Read the CSV file header
+			String header = getCSVHeader(fileReader);
 
-			// Read the file line by line starting from the second line
-			while ((line = fileReader.readLine()) != null) {
-				// Get all tokens available in line
-				String[] tokens = line.split(COMMA_DELIMITER);
-				if (tokens.length == 5) {
-					// Create a new product object and fill his data
-					// TODO - Use Builder Pattern. ProductDto.......build()
-					ProductDto product = new ProductDto(tokens[PRODUCT_ID_IDX], tokens[PRODUCT_DISPLAY_NAME_IDX],
-							tokens[SKU_IDX], tokens[EXP_DATE_IDX], tokens[PROD_DESCRIPTION_IDX]);
-					listProduct.add(product);
-				} else {
-					throw new ImportParseException("Column is more than expected");//TODO - Add Message
+			if (prescribedFileHeader.equalsIgnoreCase(header)) {
+
+				// Read the file line by line starting from the second line
+				while ((line = fileReader.readLine()) != null) {
+
+					// Get all tokens available in line
+					String[] tokens = line.split(COMMA_DELIMITER, -1);
+					if (0 != tokens.length) {
+						if (tokens.length == header.split(COMMA_DELIMITER).length) {
+							// Create a new product object and fill this data
+							// TODO - Use Builder Pattern. ProductDto.......build()
+							ProductDto product = new ProductDto(tokens[PRODUCT_TYPE_CODE_IDX],
+									tokens[PRODUCT_DISPLAY_NAME_IDX], tokens[SKU_IDX], tokens[EXP_DATE_IDX],
+									tokens[PROD_DESCRIPTION_IDX]);
+
+							listProduct.add(product);
+
+						}
+					} else {
+						throw new ImportParseException();
+					}
+					// TODO - Remove after UT. Print the new product list
+					for (ProductDto prod : listProduct) {
+						LOGGER.info(prod.toString());
+
+					}
+
 				}
+			} else {
+				throw new ImportParseException();
 			}
+		}
 
-			// TODO - Remove after UT. Print the new product list
-			for (ProductDto prod : listProduct) {
-				System.out.println(prod.toString());
-			}
-		} catch (ImportParseException ipe) {// TODO - Proper Handling
-			throw new ImportParseException("Error in Parsing csv Data");
-		} catch (Exception e) {
-			throw new ImportParseException("Error in Parsing csv Data");
-		} finally {
+		finally {
 			try {
 				fileReader.close();
 			} catch (IOException e) {
-				throw new ImportParseException("Error while closing fileReader !!!");
-				
+				e.printStackTrace();
+
 			}
 		}
 		return listProduct;
 
+	}
+
+	private String getCSVHeader(BufferedReader fileReader) {
+		String header = BLANK;
+		try {
+			header = fileReader.readLine();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return header;
 	}
 
 }
