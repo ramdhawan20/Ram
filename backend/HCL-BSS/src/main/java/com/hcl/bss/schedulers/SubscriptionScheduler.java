ad25d96ab7bcbeba6161f3b1d05110c61cf008a4
aditya-gu <aditya.delite@gmail.com>
2019-02-28 17:44:33 +0530

    adding exception handling for createSubscriptionBatch

--- backend/HCL-BSS/src/main/java/com/hcl/bss/schedulers/SubscriptionScheduler.java
+++ backend/HCL-BSS/src/main/java/com/hcl/bss/schedulers/SubscriptionScheduler.java
@@ -83,61 +83,103 @@ public class SubscriptionScheduler {
     }
     @Transactional(rollbackOn = {Exception.class})
     private void createCustomerAccount(Order order, Subscription subscription){
-        if(order.getIsCorporate()==1){
-            createCompany(order);
+        try{
+            if(order.getIsCorporate()==1){
+                createCompany(order);
+            }
+            else{
+                //individual customer
+                persistAddress(order);
+            }
+            Customer customer = new Customer();
+            customer.setCreatedDate(new Timestamp(System.currentTimeMillis()));
+            customer.setUpdatedDate(new Timestamp(System.currentTimeMillis()));
+            customer.setFirstName(order.getBillToFirstName());
+            customer.setLastName(order.getBillToLastName());
+            customer.setEmail(order.getBillToEmail());
+            customer.setPhone(order.getBillToPhone());
+            customer.setBillTo(dataMap.get("billTo"));
+            customer.setSoldTo(dataMap.get("soldTo"));
+            customer.setCompanyId(dataMap.get("companyId"));
+            customer.setSubscriptions(subscription);
+            if(!"Fail".equals(order.getStatus())){
+                Customer cust = customerRepository.save(customer);
+                order.setStatus("Pass");
+                orderRepository.save(order);
+            }
         }
-        Customer customer = new Customer();
-        customer.setFirstName(order.getBillToFirstName());
-        customer.setLastName(order.getBillToLastName());
-        customer.setEmail(order.getBillToEmail());
-        customer.setPhone(order.getBillToPhone());
-        customer.setBillTo(dataMap.get("billTo"));
-        customer.setSoldTo(dataMap.get("soldTo"));
-        customer.setCompanyId(dataMap.get("companyId"));
-        customer.setSubscriptions(subscription);
-        if(!"Fail".equals(order.getStatus())){
-            Customer cust = customerRepository.save(customer);
-            order.setStatus("Pass");
+        catch(Exception ex){
+            OrderErrors error = addOrderError(order, "Customer could not be created : "+ex.getMessage());
+            //OrderErrors error = addOrderError(order, "Customer could not be created");
+            order.setStatus("Fail");
+            orderErrorsRepository.save(error);
             orderRepository.save(order);
         }
-
-        //dataMap.put("customerId", cust.getId());
-        //create customer directly
     }
 
     private void createCompany(Order order){
-        Company company = new Company();
-        company.setCompanyName(order.getCompanyName());
-        //Map<String, Long> addressMap =
-        persistAddress(order);
-        company.setBillToId(dataMap.get("billTo"));
-        company.setSoldToId(dataMap.get("soldTo"));
-        Company comp = companyRepository.save(company);
-        dataMap.put("companyId",comp.getId());
+        try{
+            Company company = new Company();
+            company.setCompanyName(order.getCompanyName());
+            persistAddress(order);
+            company.setBillToId(dataMap.get("billTo"));
+            company.setSoldToId(dataMap.get("soldTo"));
+            company.setCreatedDate(new Timestamp(System.currentTimeMillis()));
+            company.setUpdatedDate(new Timestamp(System.currentTimeMillis()));
+            Company comp = companyRepository.save(company);
+            dataMap.put("companyId",comp.getId());
+        }
+        catch(Exception ex){
+            //OrderErrors error = addOrderError(order, "Company could not be created : "+ex.getMessage());
+            OrderErrors error = addOrderError(order, "Company could not be created");
+            order.setStatus("Fail");
+            orderErrorsRepository.save(error);
+            orderRepository.save(order);
+        }
     }
 
 
     private Map<String,Long> persistAddress(Order order){
-        Address billToAddr = new Address();
-        Address soldToAddr = new Address();
-        billToAddr.setLine1(order.getBillToAddrLine1());
-        billToAddr.setLine2(order.getBillToAddrLine2());
-        billToAddr.setCity(order.getBillToCity());
-        billToAddr.setState(order.getBillToState());
-        Country country = entityManager.getReference(Country.class,order.getBillToCountry());
-        billToAddr.setCountry(country);
+        try{
+            Address billToAddr = new Address();
+            Address soldToAddr = new Address();
+            billToAddr.setLine1(order.getBillToAddrLine1());
+            billToAddr.setLine2(order.getBillToAddrLine2());
+            billToAddr.setCity(order.getBillToCity());
+            billToAddr.setState(order.getBillToState());
+            billToAddr.setCreatedDate(new Timestamp(System.currentTimeMillis()));
+            billToAddr.setUpdatedDate(new Timestamp(System.currentTimeMillis()));
+            Country country = entityManager.find(Country.class,order.getBillToCountry());
+            if(country==null){
+                OrderErrors error = addOrderError(order, "Country: "+ order.getBillToCountry() + " is not configured");
+                order.setStatus("Fail");
+                orderErrorsRepository.save(error);
+                orderRepository.save(order);
+            }
+            billToAddr.setCountry(country);
 
-        soldToAddr.setLine1(order.getSoldToAddrLine1());
-        soldToAddr.setLine2(order.getSoldToAddrLine2());
-        soldToAddr.setCity(order.getSoldToCity());
-        soldToAddr.setState(order.getSoldToState());
-        soldToAddr.setCountry(country);
+            soldToAddr.setLine1(order.getSoldToAddrLine1());
+            soldToAddr.setLine2(order.getSoldToAddrLine2());
+            soldToAddr.setCity(order.getSoldToCity());
+            soldToAddr.setState(order.getSoldToState());
+            soldToAddr.setCreatedDate(new Timestamp(System.currentTimeMillis()));
+            soldToAddr.setUpdatedDate(new Timestamp(System.currentTimeMillis()));
+            soldToAddr.setCountry(country);
 
-        Address ad1 = addressRepository.save(billToAddr);
-        Address ad2 = addressRepository.save(soldToAddr);
-        dataMap.put("billTo", ad1.getId());
-        dataMap.put("soldTo", ad2.getId());
-        return dataMap;
+            Address ad1 = addressRepository.save(billToAddr);
+            Address ad2 = addressRepository.save(soldToAddr);
+            dataMap.put("billTo", ad1.getId());
+            dataMap.put("soldTo", ad2.getId());
+            return dataMap;
+        }
+        catch(Exception ex){
+            //OrderErrors error = addOrderError(order,"Bill To and Sold To could not be persisted due to:"+ ex.getMessage());
+            OrderErrors error = addOrderError(order,"Bill To and Sold To could not be persisted ");
+            order.setStatus("Fail");
+            orderErrorsRepository.save(error);
+            orderRepository.save(order);
+            return null;
+        }
     }
 
     /**
@@ -196,9 +238,13 @@ public class SubscriptionScheduler {
             return subscription;
         }
         catch(Exception ex){
-            //TODO
+            //OrderErrors error = addOrderError(order, "Subscription could not be created "+ex.getMessage());
+            OrderErrors error = addOrderError(order, "Subscription could not be created ");
+            order.setStatus("Fail");
+            orderErrorsRepository.save(error);
+            orderRepository.save(order);
+            return null;
         }
-        return null;
     }
 
 
@@ -207,7 +253,7 @@ public class SubscriptionScheduler {
         subRatePlan.setBillingCycle(order.getBillingCycle());
         PricingScheme pricingScheme = entityManager.find(PricingScheme.class, order.getPricingSchemeCode());
         if(pricingScheme== null){
-            OrderErrors error = addOrderError(order,order.getPricingSchemeCode()+ " is not configured");
+            OrderErrors error = addOrderError(order,"Pricing Scheme Code: "+order.getPricingSchemeCode()+ " is not configured");
             order.setStatus("Fail");
             orderErrorsRepository.save(error);
             orderRepository.save(order);
@@ -220,6 +266,8 @@ public class SubscriptionScheduler {
             subRatePlan.setRatePlan(ratePlan.getId());
             subRatePlan.setProduct(order.getProductId());
             subRatePlan.setPricingScheme(pricingScheme);
+            subRatePlan.setCreatedDate(new Timestamp(System.currentTimeMillis()));
+            subRatePlan.setUpdatedDate(new Timestamp(System.currentTimeMillis()));
             if("VOLUME".equals(pricingScheme.getPricingSchemeCode())){
                 int quantity = order.getQuantity();
                 if(quantity<0){
@@ -242,7 +290,6 @@ public class SubscriptionScheduler {
             }
         }
         return subRatePlan;
-
     }
     /**
      * This method checks if the product present in Order is configured in the product table
@@ -274,6 +321,8 @@ public class SubscriptionScheduler {
         OrderErrors errors = new OrderErrors();
         errors.setErrorDesc(errorMsg);
         errors.setOrder(order);
+        errors.setCreatedDate(new Timestamp(System.currentTimeMillis()));
+        errors.setUpdatedDate(new Timestamp(System.currentTimeMillis()));
         return errors;
     }
 
@@ -319,17 +368,27 @@ public class SubscriptionScheduler {
     }
 
     private String generateSubscriptionId(Order order){
-        Product product = entityManager.find(Product.class,order.getProductId());
-        String sku = product.getSku();
-        RatePlan ratePlan = entityManager.find(RatePlan.class,order.getRatePlanId());
-        String ratePlanName = ratePlan.getRatePlanId();
-        Calendar cal = Calendar.getInstance();
-        int month = cal.get(Calendar.MONTH);
-        int year = cal.get(Calendar.YEAR);
-        StringBuilder builder = new StringBuilder();
-        int sequenceNumber = subscriptionRepository.getSubsSeq();
-        builder.append(sku).append(ratePlanName).append(month).append(year).append(sequenceNumber);
-        return builder.toString();
+        try{
+            Product product = entityManager.find(Product.class,order.getProductId());
+            String sku = product.getSku();
+            RatePlan ratePlan = entityManager.find(RatePlan.class,order.getRatePlanId());
+            String ratePlanName = ratePlan.getRatePlanId();
+            Calendar cal = Calendar.getInstance();
+            int month = cal.get(Calendar.MONTH);
+            int year = cal.get(Calendar.YEAR);
+            StringBuilder builder = new StringBuilder();
+            int sequenceNumber = subscriptionRepository.getSubsSeq();
+            builder.append(sku).append(ratePlanName).append(month).append(year).append(sequenceNumber);
+            return builder.toString();
+        }
+        catch(Exception ex){
+            order.setStatus("Fail");
+            //OrderErrors error = addOrderError(order,"Exception occured while generating subscriptionID:"+ ex.getMessage());
+            OrderErrors error = addOrderError(order,"Exception occured while generating subscriptionID:");
+            orderErrorsRepository.save(error);
+            orderRepository.save(order);
+            return "";
+        }
     }
 
 
