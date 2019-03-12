5f078979520f7a76f0f973412da996d1f7bf8ae4
Dhiraj Srivastava <dhiraj.s@hcl.com>
2019-03-12 12:05:56 +0530

    removed duplicate entry of status

--- backend/HCL-BSS/src/main/java/com/hcl/bss/domain/Subscription.java
+++ backend/HCL-BSS/src/main/java/com/hcl/bss/domain/Subscription.java
@@ -61,9 +61,6 @@ public class Subscription implements Serializable {
     @Column(name="STATUS")
     private String status;
 
-    @Column(name="STATUS")
-    private Integer quantity;
-
     @GeneratorType(type = LoggedUserGenerator.class)
     @Column(name = "CRE_BY")
     private String createdBy;
@@ -172,14 +169,6 @@ public class Subscription implements Serializable {
 
     public void setStatus(String status) {
         this.status = status;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
     }
 
     public Timestamp getCreatedDate() {
