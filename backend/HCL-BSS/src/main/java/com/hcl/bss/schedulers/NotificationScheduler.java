1301e2781eee376b6068b5b366ada0f751f83c18
Dhiraj Srivastava <dhiraj.s@hcl.com>
2019-03-12 16:24:57 +0530

    removed unwanted files

--- backend/HCL-BSS/src/main/java/com/hcl/bss/schedulers/NotificationScheduler.java
+++ backend/HCL-BSS/src/main/java/com/hcl/bss/schedulers/NotificationScheduler.java
@@ -1,4 +1,45 @@
 package com.hcl.bss.schedulers;
 
+
+/*
+import org.apache.activemq.camel.component.ActiveMQComponent;
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+*/
+
+/**
+ *
+ * author dhiraj.s
+ *
+ * Description
+ * Core responsiblity of this scheduler is to send notificaiton
+ * whether its SMS / Email.
+ * Scheduler will populate the records in EMAIL_NOTIFICATION_STATUS and SMS_NOTIFICATION_STATUS
+ * tables.
+ *
+ */
 public class NotificationScheduler {
+
+   /* public static void main(String[] args) throws Exception {
+        CamelContext context = new DefaultCamelContext();
+        try {
+            context.addComponent("activemq", ActiveMQComponent.activeMQComponent("vm://localhost?broker.persistent=false"));
+            context.addRoutes(new RouteBuilder() {
+                @Override
+                public void configure() throws Exception {
+                    from("activemq:queue:test.queue")
+                            .to("stream:out");
+                }
+            });
+            ProducerTemplate template = context.createProducerTemplate();
+            context.start();
+            template.sendBody("activemq:test.queue", "Hello World");
+            Thread.sleep(2000);
+        } finally {
+            context.stop();
+        }
+    }
+*/
 }
