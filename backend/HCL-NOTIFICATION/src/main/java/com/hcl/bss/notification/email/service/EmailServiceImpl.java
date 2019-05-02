1b5befb452b83299716a5d84f9c972af0a67442c
ranjankumar.y <ranjankumar.y@LP-5CD7490BN9.HCLT.CORP.HCL.IN>
2019-05-02 15:06:08 +0530

    email template with routing

--- backend/HCL-NOTIFICATION/src/main/java/com/hcl/bss/notification/email/service/EmailServiceImpl.java
+++ backend/HCL-NOTIFICATION/src/main/java/com/hcl/bss/notification/email/service/EmailServiceImpl.java
@@ -1,30 +1,50 @@
 package com.hcl.bss.notification.email.service;
 
+import javax.jms.ConnectionFactory;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.ProducerTemplate;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.impl.DefaultCamelContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hcl.bss.notification.client.BSSClient;
 import com.hcl.bss.notification.dto.CustomerDto;
+import com.hcl.bss.notification.email.sender.EmailNotificationProducer;
 import com.hcl.bss.notification.routes.NotificationSender;
+import com.hcl.bss.notification.routes.SampleCamelRouter;
 @Service
 public class EmailServiceImpl implements EmailService{
 @Autowired
 BSSClient bSSClient;
 @Autowired
 NotificationSender notificationSender;
+private TemplateEngine templateEngine;
+
+@Autowired
+public EmailServiceImpl(TemplateEngine templateEngine) {
+    this.templateEngine = templateEngine;
+}
+@Autowired
+EmailNotificationProducer emailNotificationProducer;
 	@Override
 	public void emailSubscriptionDetail(String subId) {
 		
 		//without camel routing
 		CustomerDto cus = bSSClient.getSubscriptionDetails(subId);
-		notificationSender.createTopic(cus);
+		//notificationSender.createTopic(cus);
 		
 		
 		
 		
 		// camel routing work and further process
+		
 	
-	/* SampleCamelRouter routeBuilder = new SampleCamelRouter();
+	 SampleCamelRouter routeBuilder = new SampleCamelRouter(emailNotificationProducer);
      //CreateTopicRouteBuilder createTopicRouteBuilder = new CreateTopicRouteBuilder();
      CamelContext ctx = new DefaultCamelContext();
 
@@ -39,11 +59,11 @@ NotificationSender notificationSender;
          ObjectMapper mapper = new ObjectMapper();
          String jsonString = mapper.writeValueAsString(cus);
          template.sendBody("jms:topic:subscriptionr-topic",jsonString);
-         Thread.sleep(5 * 60 * 1000);
+       //  Thread.sleep(5 * 60 * 1000);
          ctx.stop();
      }
      catch (Exception e) {
          e.printStackTrace();
-     }*/
+     }
 	}
 }
